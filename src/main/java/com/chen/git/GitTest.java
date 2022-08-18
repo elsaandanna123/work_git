f204dd42c4c83897068056e534174a74765ca6b0
elsaandanna123 <45280300+elsaandanna123@users.noreply.github.com>
2022-08-18 16:20:27 +0800

    Update GitTest.java

--- src/main/java/com/chen/git/GitTest.java
+++ src/main/java/com/chen/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hellogit");
         System.out.println("hellogit2");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
