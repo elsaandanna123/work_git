8760b3508d4844919ae9658d441f733045f468ef
Chen <741881748@qq.com>
2022-08-18 15:04:56 +0800

    hot-fix commit

--- src/main/java/com/chen/git/GitTest.java
+++ src/main/java/com/chen/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hellogit");
         System.out.println("hellogit");
         System.out.println("hellogit");
+        System.out.println("hellogit2");
     }
 }
